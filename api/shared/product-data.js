6791127ddfbdd478fa7e8f4cf0a9f6c14fae1075
Medhat Elmasry <medhat@elmasry.ca>
2020-07-19 11:21:36 -0700

    get one item

--- api/shared/product-data.js
+++ api/shared/product-data.js
@@ -2,31 +2,31 @@ const CosmosClient = require("@azure/cosmos").CosmosClient;
 const DbContext = require("../data/data-context");
 const SeedData = require("../data/seed-data");
 
-const { ENDPOINT, KEY, DATABASE, CONTAINER, PARTITION_KEY} = process.env;
+const { ENDPOINT, KEY, DATABASE, CONTAINER, PARTITION_KEY } = process.env;
 const client = new CosmosClient({ endpoint: ENDPOINT, key: KEY });
 const database = client.database(DATABASE);
 const container = database.container(CONTAINER);
 
 const seedProducts = async () => {
-  let partition_key =JSON.parse(PARTITION_KEY);
+  let partition_key = JSON.parse(PARTITION_KEY);
   await DbContext.create(client, DATABASE, CONTAINER, partition_key);
 
   let iterator = container.items.readAll();
-    let { resources } = await iterator.fetchAll();
+  let { resources } = await iterator.fetchAll();
 
-    if (resources.length > 0) {
-      return {"message": `The database is already seeded with ${resources.length} products.`};
-    } else {
-      
-      const products = SeedData.getSeedProductData();
+  if (resources.length > 0) {
+    return { "message": `The database is already seeded with ${resources.length} products.` };
+  } else {
 
-      products.forEach(async function (item) {
-        const { resource: createdItem } = await container.items.create(item);
-        console.log(item);
-      })
+    const products = SeedData.getSeedProductData();
 
-      return {"message": `The database has been seeded with ${products.length} products.`};
-    }
+    products.forEach(async function (item) {
+      const { resource: createdItem } = await container.items.create(item);
+      console.log(item);
+    })
+
+    return { "message": `The database has been seeded with ${products.length} products.` };
+  }
 };
 
 const getProducts = async () => {
@@ -54,8 +54,32 @@ const updateProduct = async (id, product) => {
   return await container.item(id, product.name).replace(product);
 };
 
-const deleteProduct = async (id,name) => {
-  return await container.item(id,name).delete();
+const readProduct = async (id) => {
+  // query to return all items
+  const querySpec = {
+    query: "SELECT * FROM c WHERE c.id='" + id + "'"
+  };
+
+  // read all items in the Items container
+  const { resources: items } = await container.items
+    .query(querySpec)
+    .fetchAll();
+
+  if (items.length > 0)
+    return items[0];
+  else
+    return {};
 };
 
-module.exports = {seedProducts, addProduct, updateProduct, deleteProduct, getProducts };
+const deleteProduct = async (id, name) => {
+  return await container.item(id, name).delete();
+};
+
+module.exports = {
+  seedProducts,
+  addProduct,
+  updateProduct,
+  deleteProduct,
+  getProducts,
+  readProduct
+};
